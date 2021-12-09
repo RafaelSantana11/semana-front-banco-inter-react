bfc267d68e86962d74e0786d35e95dadc6ef4589
RafaelSantana11 <rafael@araguaiatec.com>
2021-12-09 10:38:42 -0300

    colocando card e logo na pagina inicial

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,15 +1,19 @@
 import { Wrapper, Background, InputContainer, ButtonContainer } from "./styles";
-
+import Card from "../../components";
 import background from "../../assets/images/background-login.jpg";
+import logoInter from "../../assets/images/inter-orange.png";
 
 const SignIn = () => {
   return (
     <Wrapper>
       <Background image={background} />
+      <Card width="403px" height="auto">
+        <img src={logoInter} width={172} height={61} alt="logo inter" />
+      </Card>
     </Wrapper>
   );
 };
 
-//26:29
+//41:42
 
 export default SignIn;
